f27d22377c630c6b73a44db73f953ef2df99597c
Rajdewarde <rajdewarde7777@gmail.com>
2026-02-23 23:43:15 +0530

    Add files via upload

---
+++ 09_UpperLowerCase.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class UpperLowerCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Uppercase: "+str.toUpperCase());
+        System.out.println("Lowercase: "+str.toLowerCase());
+        sc.close();
+    }
+}
