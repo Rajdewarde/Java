21eaabac4c9c99ee00f40ed2082bd6f82330509e
Rajdewarde <rajdewarde7777@gmail.com>
2026-02-23 23:41:05 +0530

    Add files via upload

---
+++ 03_ReverseString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String rev = new StringBuilder(str).reverse().toString();
+        System.out.println("Reverse = " + rev);
+        sc.close();
+    }
+}
