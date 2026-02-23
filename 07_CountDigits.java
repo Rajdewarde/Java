1378f4340e8456909395094fbf0a6908af07846d
Rajdewarde <rajdewarde7777@gmail.com>
2026-02-23 23:42:44 +0530

    Add files via upload

---
+++ 07_CountDigits.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count=0;
+        for(char ch: str.toCharArray())
+            if(Character.isDigit(ch)) count++;
+        System.out.println("Digits = "+count);
+        sc.close();
+    }
+}
