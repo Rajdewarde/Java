50a3f57e845b59714a3a493f3b2b49f8045db321
Rajdewarde <rajdewarde7777@gmail.com>
2026-02-23 23:42:04 +0530

    Add files via upload

---
+++ 05_CountVowels.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        int count=0;
+        for(char ch: str.toCharArray())
+            if("aeiou".indexOf(ch)!=-1) count++;
+        System.out.println("Vowels = "+count);
+        sc.close();
+    }
+}
